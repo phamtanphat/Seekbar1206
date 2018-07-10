71eab36d75dd66e75c1ec6ae737310ae63ff8e13
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-10 18:41:13 +0700

    "Thuoc tinh seekbar"

--- app/src/main/java/com/ptp/phamtanphat/seekbar1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/seekbar1206/MainActivity.java
@@ -2,12 +2,16 @@ package com.ptp.phamtanphat.seekbar1206;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar sk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sk = findViewById(R.id.seekbar);
     }
 }
